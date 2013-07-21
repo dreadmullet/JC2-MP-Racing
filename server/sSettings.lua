3d2b7449446fa9a129f0b80f5274e760942c60d0
dreadmullet <loosepolygon@gmail.com>
2013-07-21 09:38:11 -0400

    Version --> 0.6.2.

--- server/sSettings.lua
+++ server/sSettings.lua
@@ -11,7 +11,7 @@ settings.debugLevel = 1
 
 settings.name = "JC2-MP-Racing"
 settings.description = "Race cars and shit."
-settings.version = "0.6.1"
+settings.version = "0.6.2"
 if settings.debugLevel > 1 then
 	settings.version = settings.version.." (debug)"
 end
