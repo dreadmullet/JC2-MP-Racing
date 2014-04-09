6ecd91bb72fc329e6157a6a8c57bb6c7eaa240e2
dreadmullet <loosepolygon@gmail.com>
2014-04-09 00:40:10 +0000

    EGUSM: Added SubscribeUtility console functionality.

--- shared/EGUSM/SubscribeUtility.lua
+++ shared/EGUSM/SubscribeUtility.lua
@@ -5,14 +5,17 @@ function EGUSM.SubscribeUtility:__init()
 	self.EventSubscribe = EGUSM.SubscribeUtility.EventSubscribe
 	self.NetworkSubscribe = EGUSM.SubscribeUtility.NetworkSubscribe
 	self.NetSubscribe = self.NetworkSubscribe
+	self.ConsoleSubscribe = EGUSM.SubscribeUtility.ConsoleSubscribe
 	
 	self.EventUnsubscribe = EGUSM.SubscribeUtility.EventUnsubscribe
 	self.NetworkUnsubscribe = EGUSM.SubscribeUtility.NetworkUnsubscribe
 	self.NetUnsubscribe = self.NetworkUnsubscribe
+	self.ConsoleUnsubscribe = EGUSM.SubscribeUtility.ConsoleUnsubscribe
 	
 	self.EventUnsubscribeAll = EGUSM.SubscribeUtility.EventUnsubscribeAll
 	self.NetworkUnsubscribeAll = EGUSM.SubscribeUtility.NetworkUnsubscribeAll
 	self.NetUnsubscribeAll = self.NetworkUnsubscribeAll
+	self.ConsoleUnsubscribeAll = EGUSM.SubscribeUtility.ConsoleUnsubscribeAll
 	
 	self.Destroy = EGUSM.SubscribeUtility.Destroy
 	self.UnsubscribeAll = self.Destroy
@@ -20,18 +23,22 @@ function EGUSM.SubscribeUtility:__init()
 	-- Each element is like {eventName , event}.
 	self.subscribeUtilityEventSubs = {}
 	self.subscribeUtilityNetSubs = {}
+	self.subscribeUtilityConsoleSubs = {}
 end
 
 function EGUSM.SubscribeUtility:EventSubscribe(eventName , optionalFunction)
 	local eventSub = Events:Subscribe(eventName , self , optionalFunction or self[eventName])
 	table.insert(self.subscribeUtilityEventSubs , {eventName , eventSub})
-	return eventSub
 end
 
 function EGUSM.SubscribeUtility:NetworkSubscribe(netName , optionalFunction)
 	local eventSub = Network:Subscribe(netName , self , optionalFunction or self[netName])
 	table.insert(self.subscribeUtilityNetSubs , {netName , eventSub})
-	return eventSub
+end
+
+function EGUSM.SubscribeUtility:ConsoleSubscribe(name , optionalFunction)
+	local eventSub = Console:Subscribe(name , self , optionalFunction or self[name])
+	table.insert(self.subscribeUtilityConsoleSubs , {name , eventSub})
 end
 
 function EGUSM.SubscribeUtility:EventUnsubscribe(eventName)
@@ -54,6 +61,16 @@ function EGUSM.SubscribeUtility:NetworkUnsubscribe(netName)
 	end
 end
 
+function EGUSM.SubscribeUtility:ConsoleUnsubscribe(name)
+	for n = #self.subscribeUtilityConsoleSubs , 1 , -1 do
+		local pair = self.subscribeUtilityConsoleSubs[n]
+		if pair[1] == name then
+			Console:Unsubscribe(pair[2])
+			table.remove(self.subscribeUtilityConsoleSubs , n)
+		end
+	end
+end
+
 function EGUSM.SubscribeUtility:EventUnsubscribeAll()
 	for index , pair in ipairs(self.subscribeUtilityEventSubs) do
 		Events:Unsubscribe(pair[2])
@@ -68,7 +85,15 @@ function EGUSM.SubscribeUtility:NetworkUnsubscribeAll()
 	self.subscribeUtilityNetSubs = {}
 end
 
+function EGUSM.SubscribeUtility:ConsoleUnsubscribeAll()
+	for index , pair in ipairs(self.subscribeUtilityConsoleSubs) do
+		Console:Unsubscribe(pair[2])
+	end
+	self.subscribeUtilityConsoleSubs = {}
+end
+
 function EGUSM.SubscribeUtility:Destroy()
 	self:EventUnsubscribeAll()
 	self:NetworkUnsubscribeAll()
+	self:ConsoleUnsubscribeAll()
 end
